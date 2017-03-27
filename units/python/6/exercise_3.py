22f3d38c20b369310a84569b2da5d5331be034ee
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-03-27 22:48:04 +0545

    Implement function to compute intersection point of two lines

--- units/python/6/exercise_3.py
+++ units/python/6/exercise_3.py
@@ -20,22 +20,39 @@ def parse_line(equation):
     return (float(m), float(c))
 
 
+def intersection_point(line1, line2):
+    """
+    Calculates the intersection point of two lines given by their 
+    parsed tuple containing slope & y-intercept. 
+    """
+    (m1, c1) = line1
+    (m2, c2) = line2
+
+    # Get the point of intersection
+    x = (c1 - c2) / (m2 - m1)
+    y = m1 * x + c1             # As we have y = mx + c
+
+    return (x, y)
+
+
 def main():
     """
     The main function that actually executes the program.
     """
-    
-    print('Enter equations of lines in y = mx + c form:')
+
+    print('Enter equations of lines in y = mx + c form:\n')
 
     # Get the equations of two lines from the user.
     equation1 = input('Line 1: ')
     equation2 = input('Line 2: ')
 
+    # Parse the two equations of two lines.
     line1 = parse_line(equation1)
     line2 = parse_line(equation2)
 
-    print('Line 1: ', line1)
-    print('Line 2: ', line2)
+    # Print the intersection point.
+    point = intersection_point(line1, line2)
+    print('\nIntersection Point: (x, y) = ({}, {})'.format(*point))
 
 
 # Run the program
