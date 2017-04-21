cc93f876784361a671da659ad203ddbb92b289ed
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-04-21 06:19:26 +0545

    Add the setTimeout example

--- units/web/2/examples/hello-world/js/hello.js
+++ units/web/2/examples/hello-world/js/hello.js
@@ -1,5 +1,11 @@
 window.addEventListener('load', handleLoad);
 
 function handleLoad() {
+    // This will invoke the changeContent function after 3 seconds.
+    setTimeout(changeContent, 3000);
+}
+
+function changeContent() {
+    // This will change the content of the <p> tag.
     document.querySelector('p').innerHTML = "I'm learning Web Development with HTML, CSS and JavaScript.";
 }
